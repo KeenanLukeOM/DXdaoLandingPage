163a87cbb0c1c1730d0720860b56fad6ad7e156e
Federico Luzzi <fedeluzzi00@gmail.com>
2021-11-05 12:16:44 +0100

    chore: webpack config override

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,55 @@
 const globby = require("globby");
 const fs = require("fs");
 const util = require("util");
+const { GitRevisionPlugin } = require("git-revision-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const renameFile = util.promisify(fs.rename);
+
+exports.onCreateWebpackConfig = ({ stage, getConfig, actions }) => {
+  if (stage !== "build-javascript") return;
+
+  const gitRevisionPlugin = new GitRevisionPlugin();
+  const shortCommitHash = gitRevisionPlugin.commithash().substring(0, 8);
+
+  const config = getConfig();
+  config.output.filename = `static/js/[name].${shortCommitHash}.js`;
+  config.output.chunkFilename = `static/js/[name].${shortCommitHash}.chunk.js`;
+
+  const correctPlugins = [];
+  for (let i = 0; i < config.plugins.length; i++) {
+    const plugin = config.plugins[i];
+    if (plugin instanceof MiniCssExtractPlugin)
+      correctPlugins.push(
+        new MiniCssExtractPlugin({
+          filename: `static/css/[name].${shortCommitHash}.css`,
+          chunkFilename: `static/css/[name].${shortCommitHash}.chunk.css`,
+        })
+      );
+    else correctPlugins.push(plugin);
+  }
+  config.plugins = correctPlugins;
+
+  for (let i = 0; i < config.module.rules.length; i++) {
+    if (config.module.rules[i].use && config.module.rules[i].use.length) {
+      for (let j = 0; j < config.module.rules[i].use.length; j++) {
+        if (
+          config.module.rules[i].use[j].loader ===
+            require.resolve("file-loader") ||
+          config.module.rules[i].use[j].loader === require.resolve("url-loader")
+        ) {
+          config.module.rules[i].use[j].options.name =
+            "static/media/[name].[ext]";
+        }
+      }
+    }
+  }
+  config.optimization.moduleIds = "named";
+
+  actions.replaceWebpackConfig(config);
+};
 
 exports.onPostBuild = async () => {
   const paths = await globby([
